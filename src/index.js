fe2e4baa8386f307eabe9ea17821c07e16a691ab
ky <kevinyauuuu@gmail.com>
2020-01-19 01:14:41 +0800

    update import path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { red, amber } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
